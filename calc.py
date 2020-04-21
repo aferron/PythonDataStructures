403aebc203974a6161c55ab3694210d90f38b2c7
amifer <44795728+aferron@users.noreply.github.com>
2020-04-20 23:36:43 -0700

    Sequences

--- calc.py
+++ calc.py
@@ -21,7 +21,21 @@ def eulers(y, h, t_start, t_end):
         print("n:", n, "\tt:{:.4}".format(t), "\ty:{:.6}".format(y))
 
 
-    
+# Newton's Method to approximate sqrt 2
+def newtons(prev_solution):
+    return float(prev_solution + float(2 / prev_solution)) / 2
+
+def recursive_sequence(n, f_x):
+    if n == 1:
+        # a_1 = 1
+        return 1
+    prev_solution = recursive_sequence(n - 1, f_x)
+    return f_x(prev_solution)
+
+
+
+
+print("sqrt 2 is approximately: ", recursive_sequence(10, newtons))    
 
 #print("dydt: ", dydt(4.7, 16))
-eulers(16, .2, 4.7, 5.7)
+#eulers(16, .2, 4.7, 5.7)
